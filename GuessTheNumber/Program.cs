357aebfb144cb182b7414c24f2ad705a11fce561
Nicholas Moore <nick_r_moore@hotmail.com>
2021-04-17 22:39:08 -0400

    Preparing for the guess.

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -6,7 +6,23 @@ namespace GuessTheNumber
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a number between 1 and 1000.");
+
+            // This will be for the higher/lower section.
+            // Read the keypress
+            // var kp = Console.ReadKey(false).Key;
+            // switch (kp)
+            // {
+               //  case ConsoleKey.UpArrow:
+                  //   Console.WriteLine("Up");
+                    // break;
+                // case ConsoleKey.DownArrow:
+                   //  Console.WriteLine("Down");
+                    // break;
+                // default:
+                   //  Console.WriteLine("Please press the up or down arrow.");
+                    // break;
+            // }
         }
     }
 }
